92847131849b8684566c125f35c2a0217545b330
Montego Germany <tegogermany@gmail.com>
2019-02-01 12:20:08 -0700

    For loop exercises

--- exercises/for-loop-olympics/olympic.js
+++ exercises/for-loop-olympics/olympic.js
@@ -59,3 +59,112 @@
 //     }
 // }
 //  console.log(trashIt)
+
+
+
+//             Silver Medal
+
+
+
+//                 1
+
+// var peopleArray = [
+//     {
+//       name: "Harrison Ford",
+//       occupation: "Actor"
+//     },
+//     {
+//       name: "Justin Bieber",
+//       occupation: "Singer"
+//     },
+//     {
+//       name: "Vladimir Putin",
+//       occupation: "Politician"
+//     },
+//     {
+//       name: "Oprah",
+//       occupation: "Entertainer"
+//     }
+//   ]
+
+//   for(i = 0; i < peopleArray.length; i ++) {
+//     console.log(peopleArray[i].name)
+//   }
+
+
+//                2  & 3
+
+
+// var peopleArray = [
+//     {
+//       name: "Harrison Ford",
+//       occupation: "Actor"
+//     },
+//     {
+//       name: "Justin Bieber",
+//       occupation: "Singer"
+//     },
+//     {
+//       name: "Vladimir Putin",
+//       occupation: "Politician"
+//     },
+//     {
+//       name: "Oprah",
+//       occupation: "Entertainer"
+//     }
+//   ]
+
+// var occupations = []
+// var names = []
+//   for(i = 0; i < peopleArray.length; i ++){
+     //               2
+    //   names.push(peopleArray[i].name)
+    //          
+    //                3
+    //   occupations.push(peopleArray[i].occupation)
+    //   }
+    //          2
+    //   console.log(names)
+
+    //           3
+    //   console.log(occupations)
+
+// console
+
+
+
+
+//                  Gold
+
+
+//              1
+
+var gld = []
+
+
+// for(var i = 0; i < 3; i ++){
+//     gld.push([])
+//     for (var a = 0; a < 3; a ++){
+//         gld[i].push(0)
+//     }
+// }
+// console.log(gld)
+
+//            2
+
+// for(var i = 0; i < 3; i ++){
+//     gld.push([i, i, i])
+    
+// }
+
+// console.log(gld)
+
+//          3
+
+for(var i = 0; i < 3; i ++){
+    gld.push([])
+    for(var a = 0; a < 3; a ++){
+        gld[i].push('x')
+    }
+}
+console.log(gld)
