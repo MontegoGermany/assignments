c1d0ae264a83152408961896f768fa2e2c9e39b9
Montego Germany <tegogermany@gmail.com>
2019-02-19 16:07:14 -0700

    intro to react

---
+++ exercises/react-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+import "./App.css"
+
+const App = () => {
+    return(
+        <div>
+            Whats Good World
+            <Navbar />
+            <Navbar />
+            <Navbar />
+        </div>
+    )
+}
+
+export default App;
---
+++ exercises/react-app/src/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+                Header
+            </h1>
+            <ul>
+                <li>About</li>
+                <li>Home</li>
+                <li>Comtact</li>
+               
+
+            </ul>
+        </div>
+    )
+}
+
+
+export default App;
