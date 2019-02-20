948803051c48a7eb81516dea41504415c14b8945
Montego Germany <tegogermany@gmail.com>
2019-02-20 09:04:22 -0700

    exercises

---
+++ exercises/react-biz/src/App.js
@@ -0,0 +1,2 @@
+import React from "react";
+import CSS from "./App.css"
---
+++ exercises/react-biz/src/index.js
@@ -0,0 +1,12 @@
+// 1. A nav bar
+// 2. An info section
+// 3. A list of products or services
+// 4. Images
+// 5. A footer
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+
+
